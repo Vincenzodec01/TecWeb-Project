25016a8abfc6987d9ab1801a87e38ecd66d95c60
Vincenzodec01 <150331519+Vincenzodec01@users.noreply.github.com>
2024-01-05 23:49:11 +0100

    Update add_clean_room.sql

--- dormitoryproject/database/cleaner/add_clean_room.sql
+++ dormitoryproject/database/cleaner/add_clean_room.sql
@@ -2,5 +2,3 @@ CREATE PROCEDURE add_clean_room(IN _id_staff INT, IN _room_number CHAR(3))
 BEGIN
     INSERT INTO cleaning(cleaning_id, cleaning_date, room_number, id_staff) VALUES (0, NOW(), _room_number, _id_staff);
 end;
-
-call add_clean_room(null, '200');
